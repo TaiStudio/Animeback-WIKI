d67ace51d2069ea35e26afd320dee6b316e99a38
Tai Studio <tai.studio@outlook.fr>
2021-08-01 00:18:57 +0200

    🖋 Update

---
+++ docusaurus.config.js
@@ -0,0 +1,103 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Animeback WIKI',
+  tagline: 'Anime your wallpapers',
+  url: 'https://wiki.animeback.tai-studio.ml/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'TaiStudio', // Usually your GitHub org/user name.
+  projectName: 'Animeback', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Animeback WIKI',
+      logo: {
+        alt: 'Animeback WIKI Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {href: 'https://animeback.tai-studio.ml/', label: 'Download', position: 'left'},
+        {
+          href: 'https://github.com/LeGitHubDeTai/AnimeBack',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Submit Extension',
+              href: 'https://github.com/TaiStudio/animeback-submit',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discord.com/invite/zctFdAPUzP',
+            },
+            {
+              label: 'Download',
+              href: 'https://animeback.tai-studio.ml/',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Tai Studio',
+              href: 'https://tai-studio.ml/',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/LeGitHubDeTai/AnimeBack',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Animeback, Tai Studio.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
